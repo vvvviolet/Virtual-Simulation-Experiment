4dc8f59988ce1fd72af3d985925d4dfde09bbf61
climoiiin <1309824959@qq.com>
2023-03-18 17:16:37 +0800

    'pdf'

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Template.vue'
 import exp1_1 from '../views/exp-1-1.vue'
 import exp1_2 from '../views/exp-1-2.vue'
+import showpdf from '../views/showpdf.vue'
 
 const routes = [
   {
@@ -15,6 +16,10 @@ const routes = [
       { path:'/exp1_1',component:exp1_1 },
       { path:'/exp1_2',component:exp1_2 }
     ]
+  },
+  {
+    path:'/show',
+    component:showpdf
   }
 ]
 
--- front/src/views/exp-1-1.vue
+++ front/src/views/exp-1-1.vue
@@ -1,6 +1,12 @@
 <template>
-    <p>实验1 第一部分</p>
-    <span> {{ test }}</span>
+    <h1>实验一：软件规模估算实验</h1>
+    <p>实验简介</p>
+    <el-table :data="tableData" style="width: 100%">
+    <el-table-column prop="date" label="Date" width="180" />
+    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column prop="address" label="Address" />
+  </el-table>
+  <el-button type="primary" @click="pdfHandle">实验指导书</el-button>
 </template>
 
 <script>
@@ -9,6 +15,12 @@ export default {
     data() {
         return{
             test:'21111'
+        }
+    },
+    methods:{
+        pdfHandle(){
+          
+            window.open('/#/show',"_blank")
         }
     }
 }
