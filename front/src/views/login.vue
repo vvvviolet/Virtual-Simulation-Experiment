3328e5879288edc466ecfbdd5b6537ab6a78b61c
climoiiin <1309824959@qq.com>
2023-03-27 08:02:47 +0800

    'login'

--- front/src/views/login.vue
+++ front/src/views/login.vue
@@ -1,21 +1,22 @@
 <template>
-    <div class="login-container">
-      <el-form ref="loginForm" :model="loginForm" class="login-form">
-        <el-form-item label="邮箱" prop="email">
-          <el-input v-model="loginForm.email" placeholder="请输入邮箱"></el-input>
-        </el-form-item>
-        <el-form-item label="密码" prop="password">
-          <el-input type="password" v-model="loginForm.password" placeholder="请输入密码"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <div class="login-button-wrapper">
-            <el-button type="primary" @click="submitLoginForm">登录</el-button>
-           
-          </div>
-        </el-form-item>
-      </el-form>
-    </div>
-  </template>
+  <div class="login-container">
+    <el-form ref="loginForm" :model="loginForm" class="login-form">
+      <h1 style="text-align:center">虚拟仿真实验平台</h1>
+      <el-form-item label="邮箱" prop="email">
+        <el-input v-model="loginForm.email" placeholder="请输入邮箱"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" v-model="loginForm.password" placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <div class="login-button-wrapper">
+          <el-button type="primary" @click="submitLoginForm">登录</el-button>
+
+        </div>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
 
 <script>
 import { ref } from 'vue';
@@ -46,21 +47,24 @@ export default {
 };
 </script>
 
-  <style>
-  .login-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-  }
+<style scoped>
+.login-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
 
-  .login-form {
-    width: 400px;
-  }
-  .login-button-wrapper {
+.login-form {
+  width: 400px;
+}
+
+.login-button-wrapper {
   display: flex;
   justify-content: space-around;
   align-items: space-around;
-
 }
-  </style>
+::v-deep .el-form-item__content {
+  display:block;
+}
+</style>
