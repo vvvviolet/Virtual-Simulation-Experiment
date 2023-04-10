1979e0804574d8c7e9c820899bbde36ddef0683d
Baokker <1048098862@qq.com>
2023-04-10 22:23:34 +0800

    update, but still have some bugs

--- front/src/pages/exp6/Exp6_decision_tree/HorizontalTree.vue
+++ front/src/pages/exp6/Exp6_decision_tree/HorizontalTree.vue
@@ -1,139 +1,179 @@
 <template>
-    <a-card style="height: calc(100vh - 80px);">
-      <svg :width="width" :height="height">
-        <g :transform="`translate(${margin.left},${margin.top})`">
-          <g v-for="(link, index) in links" :key="index">
-            <path
-              :d="link.path"
-              :stroke="link.color"
-              :stroke-width="linkWidth"
-              :fill="link.fill"
-            />
-            <text
-              :x="link.textX"
-              :y="link.textY"
-              :style="{ fill: link.color }"
-              text-anchor="middle"
-              alignment-baseline="central"
-            >
-              {{ link.value }}
-            </text>
-          </g>
-          <g v-for="(node, index) in nodes" :key="index">
-            <circle
-              :cx="node.x"
-              :cy="node.y"
-              :r="nodeRadius"
-              :fill="node.color"
-              :stroke-width="nodeWidth"
-              :stroke="nodeStrokeColor"
-            />
-            <text
-              :x="node.x"
-              :y="node.y"
-              :style="{ fill: node.textColor }"
-              text-anchor="middle"
-              alignment-baseline="central"
-            >
-              {{ node.value }}
-            </text>
-            <foreignObject :x="node.x + nodeRadius + 5" :y="node.y - nodeRadius" width="200">
-              <div style="height: 100%; display: flex; align-items: center;">
-                <a-input-number style="width: 80px;" :value="node.netPresentValue" disabled />
-                <span style="margin-left: 10px;">总概率: {{ node.probability }}%</span>
-              </div>
-            </foreignObject>
-          </g>
-        </g>
-      </svg>
-    </a-card>
-  </template>
-  
-  <script>
+  <div ref="container"></div>
+</template>
+
+<script>
+  import { onMounted, onUnmounted, ref, watch } from 'vue';
   import * as d3 from 'd3';
-  import { Card, InputNumber } from 'ant-design-vue';
-  
+
   export default {
-    name: 'HorizontalTree',
-  
-    components: {
-      Card,
-      InputNumber,
-    },
-  
     props: {
-      data: {
-        type: Object,
+      dataSource: {
+        type: Array,
         required: true,
       },
     },
-  
-    data() {
+    setup(props) {
+      const defaultNodeName = 'root'; // 根节点默认名称
+
+      const treeData = {
+        name: defaultNodeName,
+        children: [],
+      };
+
+      let dataSource = props.dataSource.map(({ key, ...rest }) => rest);
+
+      const keys = Object.keys(dataSource[0]); // 获取数据源中的所有属性名称
+
+      console.log(keys);
+
+      dataSource.reduce((prev, cur) => {
+        let node = prev;
+        keys.forEach((key) => {
+          const name = `${key}:${cur[key]}`;
+          let childNode = node.children.find((child) => child.name === name);
+          if (!childNode) {
+            childNode = {
+              name,
+              children: [],
+            };
+            node.children.push(childNode);
+          }
+          node = childNode; // 将新创建的或已经存在的节点设置为当前节点
+        });
+        return prev;
+      }, treeData);
+
+      console.log(treeData);
+
+      const container = ref(null);
+      let root, svg;
+
+      // 绘制树形图
+      const calculateWidthAndDepth = (tree) => {
+        let maxWidth = 0;
+        let maxDepth = 0;
+
+        // 使用队列来遍历树形结构
+        const queue = [];
+        queue.push([tree, 0]);
+
+        while (queue.length > 0) {
+          const [node, depth] = queue.shift();
+          maxDepth = Math.max(maxDepth, depth); // 更新深度
+
+          // 如果节点有子节点，则将它们加入队列
+          if (node.children) {
+            node.children.forEach((child) => {
+              queue.push([child, depth + 1]);
+            });
+
+            // 更新最大宽度
+            maxWidth = Math.max(maxWidth, node.children.length);
+          }
+        }
+
+        return { tree_width: maxWidth, tree_depth: maxDepth };
+      };
+
+      let { tree_width, tree_depth } = calculateWidthAndDepth(treeData);
+
+      console.log(tree_width, tree_depth);
+
+      const drawTree = () => {
+        const width = tree_depth * 100 + 200;
+        const height = tree_depth * 70 + 50;
+
+        console.log(width, height);
+
+        // 创建一个树形布局
+        const treeLayout = d3.tree().size([height, width]);
+
+        // 对数据进行布局，生成树形结构
+        root = d3.hierarchy(treeData);
+
+        // 计算节点位置和连线路径
+        treeLayout(root);
+
+        // 创建一个 SVG 元素，并设置其大小和坐标系
+        svg = d3
+          .select(container.value)
+          .append('svg')
+          .attr('width', width)
+          .attr('height', height)
+          .append('g')
+          .attr('transform', `translate(20,0)`)
+
+        // 绘制连线路径
+        svg
+          .selectAll('.link')
+          .data(root.links())
+          .join('path')
+          .attr('class', 'link')
+          .attr(
+            'd',
+            d3
+              .linkHorizontal()
+              .x((d) => d.y)
+              .y((d) => d.x)
+          );
+
+        // 绘制节点
+        const node = svg
+          .selectAll('.node')
+          .data(root.descendants())
+          .join('g')
+          .attr('class', 'node')
+          .attr('transform', (d) => `translate(${d.y},${d.x})`);
+
+        // 在节点内部绘制圆形和文本
+        node.append('circle').attr('r', 6);
+
+        node
+          .append('text')
+          .attr('x', (d) => (d.children ? -5 : 5))
+          .attr('text-anchor', (d) => (d.children ? 'end' : 'start'))
+          .text((d) => d.data.name);
+      };
+
+      // 组件挂载时调用绘制函数
+      onMounted(() => {
+        drawTree();
+      });
+
+      // 监听数据源变化，并重新绘制树形图
+      watch(
+        () => props.dataSource,
+        () => {
+          // 在重新绘制前先清空 SVG 元素
+          svg.selectAll('*').remove();
+          svg.remove();
+          drawTree();
+        },
+        { deep: true }
+      );
+
+      // 组件卸载时清除 SVG 元素
+      onUnmounted(() => {
+        svg.remove();
+      });
+
       return {
-        margin: { top: 20, right: 90, bottom: 30, left: 90 },
-        width: 960,
-        height: 500,
-  
-        nodeRadius: 25,
-        nodeWidth: 2,
-        nodeStrokeColor: '#52c41a',
-  
-        linkWidth: 1.5,
-        linkColor: '#bbb',
-        linkFill: 'none',
-  
-        nodes: [],
-        links: [],
+        container,
       };
     },
-  
-    mounted() {
-      this.createTree();
-    },
-  
-    methods: {
-      createTree() {
-        const treeData = d3.hierarchy(this.data, (d) => d.children);
-        const treeLayout = d3.tree().size([this.height - this.margin.top - this.margin.bottom, this.width - this.margin.left - this.margin.right]);
-  
-        treeLayout(treeData);
-  
-        const descendants = treeData.descendants();
-        const links = treeData.links();
-  
-        this.nodes = descendants.map((node) => ({
-          x: node.y,
-          y: node.x,
-          value: node.data.value,
-          color: node.children ? '#1890ff' : '#f5222d',
-          textColor: '#fff',
-          probability: node.data.probability,
-          netPresentValue: node.data.netPresentValue,
-        }));
-  
-        this.links = links.map((link) => {
-          const points = [
-            [link.source.y, link.source.x],
-            [link.target.y, link.target.x],
-          ];
-          const diagonal = d3
-            .line()
-            .curve(d3.curveCardinal)
-            .x((d) => d[0])
-            .y((d) => d[1]);
-          const path = diagonal(points);
-  
-          return {
-            path,
-            value: link.target.data.value,
-            color: this.linkColor,
-            fill: this.linkFill,
-            textX: (link.source.y + link.target.y) / 2,
-            textY: (link.source.x + link.target.x) / 2,
-          };
-        });
-      },
-    },
   };
-  </script>
-  
+</script>
+
+<style scoped>
+  .node circle {
+    fill: #fff;
+    stroke: steelblue;
+  }
+
+  .link {
+    fill: none;
+    stroke: #ccc;
+    stroke-width: 2px;
+  }
+</style>
--- front/src/pages/exp6/Exp6_decision_tree/index.vue
+++ front/src/pages/exp6/Exp6_decision_tree/index.vue
@@ -1,78 +1,234 @@
 <template>
-  <div>
-    <a-button-group style="margin-bottom: 16px">
-      <a-button @click="addNode">添加节点</a-button>
-      <a-button @click="deleteNode">删除节点</a-button>
-    </a-button-group>
-    <HorizontalTree :data="treeData" />
+  <div class="container">
+    <h1>可编辑表格</h1>
+    <a-table :columns="columns" :pagination="false" :data-source="dataSource" bordered>
+      <template
+        v-for="(col, index) in ['uncertainty', 'value', 'probability', 'start_year', 'end_year']"
+        :key="index"
+        #[col]="{ text, record }"
+      >
+        <div>
+          <a-input
+            v-if="editableData[record.key]"
+            v-model:value="editableData[record.key][col]"
+            style="margin: -5px 0"
+          />
+          <template v-else>
+            {{ text }}
+          </template>
+        </div>
+      </template>
+      <template #operation="{ record }">
+        <div class="editable-row-operations">
+          <span v-if="editableData[record.key]">
+            <a @click="save(record.key)" style="margin: 10px">保存</a>
+            <a-popconfirm title="确定取消保存吗？" @confirm="cancel(record.key)">
+              <a>取消</a>
+            </a-popconfirm>
+          </span>
+          <span v-else>
+            <a @click="edit(record.key)" style="margin: 10px">编辑</a>
+            <a-popconfirm title="确定删除本行吗？" @confirm="deleteRow(record.key)">
+              <a>删除</a>
+            </a-popconfirm>
+          </span>
+        </div>
+      </template>
+    </a-table>
+    <div class="add-button">
+      <a-button type="primary" @click="addRow">增加一行</a-button>
+    </div>
   </div>
+
+  <HorizontalTree :dataSource="dataSource"></HorizontalTree>
 </template>
 
-<script lang="ts">
+<script>
+  import { cloneDeep } from 'lodash-es';
+  import { Table } from 'ant-design-vue';
+  import * as d3 from 'd3';
   import HorizontalTree from '@/pages/exp6/Exp6_decision_tree/HorizontalTree.vue';
+  import { reactive, ref } from 'vue';
+
   export default {
-    name: 'Exp6_decision_tree',
     components: {
+      'a-table': Table,
       HorizontalTree,
     },
     data() {
       return {
-        treeData: {
-          value: 1,
-          probability: '',
-          netPresentValue: '',
-          children: [
-            {
-              value: 2,
-              probability: '90',
-              netPresentValue: '100',
-              children: [
-                {
-                  value: 3,
-                  probability: '100',
-                  netPresentValue: '200',
-                },
-                {
-                  value: 4,
-                  probability: '80',
-                  netPresentValue: '-50',
-                },
-              ],
+        editableData: {},
+        dataSource: [
+          {
+            key: '0',
+            uncertainty: '投资',
+            value: 4000,
+            probability: 0.6,
+            start_year: 1,
+            end_year: 2,
+          },
+          {
+            key: '1',
+            uncertainty: '投资',
+            value: 4500,
+            probability: 0.4,
+            start_year: 1,
+            end_year: 2,
+          },
+          {
+            key: '2',
+            uncertainty: '年经营成本',
+            value: 1200,
+            probability: 0.3,
+            start_year: 2,
+            end_year: 7,
+          },
+          {
+            key: '3',
+            uncertainty: '年经营成本',
+            value: 1000,
+            probability: 0.4,
+            start_year: 2,
+            end_year: 7,
+          },
+          {
+            key: '4',
+            uncertainty: '年经营成本',
+            value: 900,
+            probability: 0.3,
+            start_year: 2,
+            end_year: 7,
+          },
+          {
+            key: '5',
+            uncertainty: '年销售收入',
+            value: 3500,
+            probability: 0.2,
+            start_year: 2,
+            end_year: 7,
+          },
+          {
+            key: '6',
+            uncertainty: '年销售收入',
+            value: 3000,
+            probability: 0.5,
+            start_year: 2,
+            end_year: 7,
+          },
+          {
+            key: '7',
+            uncertainty: '年销售收入',
+            value: 2700,
+            probability: 0.3,
+            start_year: 2,
+            end_year: 7,
+          },
+        ],
+        editableData: {},
+        editingKey: '',
+        columns: [
+          {
+            title: '不确定因素',
+            dataIndex: 'uncertainty',
+            width: '20%',
+            slots: {
+              customRender: 'uncertainty',
             },
-            {
-              value: 5,
-              probability: '10',
-              netPresentValue: '-300',
-              children: [
-                {
-                  value: 6,
-                  probability: '100',
-                  netPresentValue: '500',
-                },
-              ],
+          },
+          {
+            title: '取值（万元）',
+            dataIndex: 'value',
+            width: '15%',
+            slots: {
+              customRender: 'value',
             },
-          ],
-        },
+          },
+          {
+            title: '概率',
+            dataIndex: 'probability',
+            width: '15%',
+            slots: {
+              customRender: 'probability',
+            },
+          },
+          {
+            title: '开始年份',
+            dataIndex: 'start_year',
+            width: '15%',
+            slots: {
+              customRender: 'start_year',
+            },
+          },
+          {
+            title: '结束年份',
+            dataIndex: 'end_year',
+            width: '15%',
+            slots: {
+              customRender: 'end_year',
+            },
+          },
+          {
+            title: '操作',
+            dataIndex: 'operation',
+            slots: {
+              customRender: 'operation',
+            },
+          },
+        ],
       };
     },
-
     methods: {
-      addNode() {
-        const node = {
-          value: this.treeData.children.length + 1,
-          probability: '',
-          netPresentValue: '',
-        };
-        this.treeData.children.push(node);
+      edit(key) {
+        this.editableData[key] = cloneDeep(this.dataSource.filter((item) => key === item.key)[0]);
+        this.editingKey = key;
       },
-
-      deleteNode() {
-        if (this.treeData.children.length > 0) {
-          this.treeData.children.pop();
-        }
+      save(key) {
+        Object.assign(this.dataSource.filter((item) => key === item.key)[0], this.editableData[key]);
+        delete this.editableData[key];
+        this.editingKey = '';
+      },
+      cancel(key) {
+        delete this.editableData[key];
+        this.editingKey = '';
+      },
+      addRow() {
+        const key = this.dataSource.length;
+        this.dataSource.push({
+          key: key.toString(),
+          uncertainty: '',
+          value: '',
+          probability: '',
+          start_year: '',
+          end_year: '',
+        });
+      },
+      deleteRow(key) {
+        this.dataSource = this.dataSource.filter((item) => key !== item.key);
+      },
+      handleDataSourceChange(data) {
+        this.dataSource = data.map((item, index) => {
+          return {
+            uncertainty: item.uncertainty,
+            value: item.value,
+            probability: item.probability,
+            start_year: item.start_year,
+            end_year: item.end_year,
+            event: item.event,
+          };
+        });
       },
     },
   };
 </script>
+<style lang="less" scoped>
+  // .container {
+  //   max-width: 800px;
+  //   margin: 0 auto;
+  //   padding-top: 32px;
+  // }
 
-<style scoped lang="less"></style>
+  .add-button {
+    margin-top: 16px;
+    text-align: right;
+  }
+</style>
