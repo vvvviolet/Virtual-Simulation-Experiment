30138950af1355d0b7829169f009f0ede3cb06e6
KEN <1610348804@qq.com>
2023-04-16 14:49:06 +0800

    feat:更新图表数据和视觉效果

--- front/src/pages/exp6/Exp6_SAM/index.vue
+++ front/src/pages/exp6/Exp6_SAM/index.vue
@@ -5,6 +5,10 @@
       一、分析变量表
     </InputChart>
 
+    <InputChart :dataSource="dataSource1" :columns="columns2" @updateData="handleDataSource2Update">
+      二、变化幅度表
+    </InputChart>
+
     <form style="text-align: center;">
       <a-button type="primary" @click="handleSubmit">更新敏感性分析结果</a-button>
     </form>
@@ -12,20 +16,13 @@
     <!-- 敏感性分析图表 -->
     <div>
       <h2>敏感性分析龙卷风图</h2>
-      <div id="sensitivity-chart" style="width: 600px; height: 400px;text-align: center;">
+      <div id="sensitivity-chart" style="height:100%;min-height:400px;text-align: center;">
       </div>
     </div>
-
-    <InputChart :dataSource="dataSource1" :columns="columns2" @updateData="handleDataSource2Update">
-      二、变化幅度表
-    </InputChart>
-    <form style="text-align: center;">
-      <a-button type="primary" @click="handleSubmit2">更新敏感性分析结果</a-button>
-    </form>
     <!-- 折线图 -->
     <div>
       <h2>敏感性分析折线图</h2>
-      <div id="line-chart" style="width: 600px; height: 400px;text-align: center;">
+      <div id="line-chart" style="height:100%;min-height:400px;text-align: center;">
       </div>
     </div>
   </div>
@@ -56,7 +53,7 @@ export default {
       dataSource1: [
           {
             key: '0',
-            uncertainty: '投资',
+            uncertainty: '敏感性因素1',
             value: 4000,
             change_minus15:73,
             change_minus10:82,
@@ -70,7 +67,7 @@ export default {
           },
           {
             key: '2',
-            uncertainty: '年经营成本',
+            uncertainty: '敏感性因素2',
             value: 1200,
             change_minus15:151,
             change_minus10:138,
@@ -84,7 +81,7 @@ export default {
           },
           {
             key: '5',
-            uncertainty: '年销售收入',
+            uncertainty: '敏感性因素3',
             value: 3500,
             change_minus15:198,
             change_minus10:172,
@@ -208,8 +205,7 @@ export default {
 
       // 绘制竖向柱状图
       this.drawSensitivityChart()
-    },
-    handleSubmit2() {
+
       // 绘制最后的折线图
       this.drawLineChart()
     },
@@ -227,7 +223,7 @@ export default {
           },
         },
         legend: {
-          data: ['收益', '成本'],
+          data: [ '亏损','收益'],
         },
         grid: {
           left: '3%',
@@ -240,28 +236,97 @@ export default {
         },
         yAxis: {
           type: 'category',
+          inverse:true,
           data: this.analysisData.map((d) => d.name),
         },
         series: [
           {
             name: '收益',
             type: 'bar',
-            stack: '总量',
+            stack: '2%',
             label: {
               show: true,
               position: 'insideRight',
             },
-            data: this.analysisData.map((d) => d.benefit),
+            //data: this.analysisData.map((d) => d.benefit),
+            data:[102,89,80]
           },
           {
-            name: '成本',
+            name: '亏损',
             type: 'bar',
-            stack: '总量',
+            stack: '2%',
             label: {
               show: true,
               position: 'insideRight',
             },
-            data: this.analysisData.map((d) => -d.cost),
+            //data: this.analysisData.map((d) => -d.cost),
+            data:[-98,-113,-123]
+          },
+          {
+            name: '收益',
+            type: 'bar',
+            stack: '5%',
+            label: {
+              show: true,
+              position: 'insideRight',
+            },
+            //data: this.analysisData.map((d) => d.benefit),
+            data:[105,55,55]
+          },
+          {
+            name: '亏损',
+            type: 'bar',
+            stack: '5%',
+            label: {
+              show: true,
+              position: 'insideRight',
+            },
+            //data: this.analysisData.map((d) => -d.cost),
+            data:[-95,-125,-150]
+          },
+          {
+            name: '收益',
+            type: 'bar',
+            stack: '10%',
+            label: {
+              show: true,
+              position: 'insideRight',
+            },
+            //data: this.analysisData.map((d) => d.benefit),
+            data:[113,33,30]
+          },
+          {
+            name: '亏损',
+            type: 'bar',
+            stack: '10%',
+            label: {
+              show: true,
+              position: 'insideRight',
+            },
+            //data: this.analysisData.map((d) => -d.cost),
+            data:[-82,-138,-172]
+          },
+          {
+            name: '收益',
+            type: 'bar',
+            stack: '15%',
+            label: {
+              show: true,
+              position: 'insideRight',
+            },
+            //data: this.analysisData.map((d) => d.benefit),
+            data:[122,16,10]
+          },
+          {
+            name: '亏损',
+            type: 'bar',
+            stack: '15%',
+            label: {
+              show: true,
+              position: 'insideRight',
+            },
+            //data: this.analysisData.map((d) => -d.cost),
+            data:[-73,-151,-198]
           },
         ],
       }
@@ -282,7 +347,7 @@ export default {
           },
         },
         legend: {
-          data: ['A', 'B'],
+          data: ['敏感性因素1', '敏感性因素2', '敏感性因素3'],
         },
         grid: {
           left: '3%',
@@ -292,21 +357,26 @@ export default {
         },
         xAxis: {
           type: 'category',
-          data: ['A', 'B', 'C']
+          data: ['-15%', '-10%', '-5%', '-2%', 'Base', '2%', '5%', '10%', '15%']
         },
         yAxis: {
           type: 'value'
         },
         series: [
         {
-          data: [10, 22, 28, 43, 49],
+          data: [73,82,95,98,100,102,105,113,122],
           type: 'line',
-          stack: 'A'
+          stack: '敏感性因素1'
         },
         {
-          data: [5, 4, 3, 5, 10],
+          data: [151,138,125,113,100,89,55,33,16],
           type: 'line',
-          stack: 'B'
+          stack: '敏感性因素2'
+        },
+        {
+          data: [198,172,150,123,100,80,55,30,10],
+          type: 'line',
+          stack: '敏感性因素3'
         }
       ],
       }
@@ -325,10 +395,9 @@ export default {
 function doAnalysis(params) {
   // 在这里执行敏感性分析并返回结果
   const analysisData = [
-    { name: 'Parameter 1', benefit: 10, cost: 5 },
-   
-    { name: 'Parameter 2', benefit: 20, cost: 10 },
-    { name: 'Parameter 3', benefit: 30, cost: 15 },
+    { name: '敏感性因素1', benefit: 10, cost: 5 },
+    { name: '敏感性因素2', benefit: 20, cost: 10 },
+    { name: '敏感性因素3', benefit: 30, cost: 15 },
     ]
 
 return analysisData
