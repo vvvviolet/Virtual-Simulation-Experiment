c8372ef0417b01d5a3c4003593d8458178e572c3
qfccse <2544668476@qq.com>
2023-04-16 19:56:24 +0800

    table

--- front/src/pages/exp1/Exp1_MARKII/index.vue
+++ front/src/pages/exp1/Exp1_MARKII/index.vue
@@ -1,289 +1,137 @@
 <template>
+  <h1 class="title" >实验1 基于MARKII的小型软件项目规模度量实验
 
-  <h1 class="title">实验1 基于MARKII的小型软件项目规模度量实验
-
-    <span>  <el-button class="guidance" type="primary" text @click="pdfHandle"><el-icon size="25px"><Document/></el-icon>实验指导书下载</el-button></span>
+    <span>  <el-button  class="guidance" type="primary" text  @click="pdfHandle" ><el-icon size="25px"><Document /></el-icon>实验指导书下载</el-button></span>
   </h1>
-  <hr/>
+  <hr />
   <!-- <span> {{ test }}</span> -->
-  <h2>一、实验目的 </h2>
-  <p class="content">理解软件项目规模度量功能点法原理，通过实验操作掌握功能点法。
-    学生应以小组为单位，根据本小组“软件工程管理与经济”课程设计项目架构及组件等设计成果，以功能点方法测量该项目的规模(功能点数量)。
-    建议选用某一种功能点方法度量课程设计项目的功能点，并采用另外一种功能点方法或其他的软件规模度量方法对前一种方法的度量结果进行验证。
-    本实验为课内设计性实验项目，实验学时 1 学时，完成实验报告 1 学时。
+  <h2>一、实验目的  </h2>
+  <p class="content">理解软件项目规模度量功能点法原理，通过实验操作掌握功能点法。 学生应以小组为单位，根据本小组“软件工程管理与经济”课程设计项目架构及组件等设计成果，以功能点方法测量该项目的规模(功能点数量)。 建议选用某一种功能点方法度量课程设计项目的功能点，并采用另外一种功能点方法或其他的软件规模度量方法对前一种方法的度量结果进行验证。 本实验为课内设计性实验项目，实验学时 1 学时，完成实验报告 1 学时。
   </p>
-  <h2>二、实验参数 </h2>
-
-  <el-table :data="tableData" border style="width: 100%" :summary-method="data=>getSummaries(data,index)" show-summary
-            ref="detailTable">
-    <el-table-column prop="component" label="组件" width="80"/>
-    <el-table-column prop="number" label="数量" width="80"/>
-    <el-table-column label="复杂度">
-      <el-table-column label="简单">
-        <el-table-column label="计数">
-          <el-table-column prop="A" label="A"/>
-          \
-        </el-table-column>
-        <el-table-column label="权重">
-          <el-table-column prop="B" label="B"/>
-        </el-table-column>
-        <el-table-column label="功能点数">
-          <el-table-column prop="C" label="C=A*B"/>
-        </el-table-column>
-      </el-table-column>
-      <el-table-column label="平均">
-        <el-table-column label="计数">
-          <el-table-column prop="D" label="D"/>
-        </el-table-column>
-        <el-table-column label="权重">
-          <el-table-column prop="E" label="E"/>
-        </el-table-column>
-        <el-table-column label="功能点数">
-          <el-table-column prop="F" label="F=D*E"/>
-        </el-table-column>
-      </el-table-column>
-      <el-table-column label="复杂">
-        <el-table-column label="计数">
-          <el-table-column prop="G" label="G"/>
-        </el-table-column>
-        <el-table-column label="权重">
-          <el-table-column prop="H" label="H"/>
-        </el-table-column>
-        <el-table-column label="功能点数">
-          <el-table-column prop="I" label="I=G*H"/>
-        </el-table-column>
-      </el-table-column>
-    </el-table-column>
-    <el-table-column prop="nonum" label="未调整功能点数" width="80"/>
-    <!-- <el-table-column prop="name" label="Name" width="180" />
-    <el-table-column prop="address" label="Address" /> -->
-  </el-table>
-
+  <h2>二、实验参数  </h2>
   <a-table
-      :columns="columns"
-      :data-source="tableData"
-      bordered
-      size="middle"
+    :columns="columns"
+    :data-source="data"
+    bordered
+    size="middle"
+    :scroll="{ x: 'calc(700px + 50%)', y: 240 }"
   />
-
-
 </template>
-
-<script>
-import {Document} from '@element-plus/icons-vue'
-
-export default {
-  name: 'Exp1_IFPUG',
-  data() {
-    return {
-      test: '21111',
-      sum: '',
-      columns: [
+<script lang="ts">
+  import type { TableColumnsType } from 'ant-design-vue';
+  import { defineComponent } from 'vue';
+  type TableDataType = {
+    key: number;
+    name: string;
+    age: number;
+    street: string;
+    building: string;
+    number: number;
+    companyAddress: string;
+    companyName: string;
+    gender: string;
+  };
+  const columns: TableColumnsType = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      width: 100,
+      fixed: 'left',
+      filters: [
         {
-          title: '组件',
-          dataIndex: 'component'
+          text: 'Joe',
+          value: 'Joe',
         },
         {
-          title: '数量',
-          dataIndex: 'num'
+          text: 'John',
+          value: 'John',
+        },
+      ],
+      onFilter: (value: string, record: TableDataType) => record.name.indexOf(value) === 0,
+    },
+    {
+      title: 'Other',
+      children: [
+        {
+          title: 'Age',
+          dataIndex: 'age',
+          key: 'age',
+          width: 200,
+          sorter: (a: TableDataType, b: TableDataType) => a.age - b.age,
         },
         {
-          title: '复杂度',
+          title: 'Address',
           children: [
             {
-              title: '简单',
-              children: [
-                {
-                  title: '计数',
-                  dataIndex: 'eCount'
-                },
-                {
-                  title: '权重',
-                  dataIndex: 'eWeight'
-                },
-                {
-                  title: '功能点数',
-                  dataIndex: 'eNum'
-                }
-              ]
+              title: 'Street',
+              dataIndex: 'street',
+              key: 'street',
+              width: 200,
             },
             {
-              title: '平均',
+              title: 'Block',
               children: [
                 {
-                  title: '计数',
-                  dataIndex: 'mCount'
+                  title: 'Building',
+                  dataIndex: 'building',
+                  key: 'building',
+                  width: 100,
                 },
                 {
-                  title: '权重',
-                  dataIndex: 'mWeight'
+                  title: 'Door No.',
+                  dataIndex: 'number',
+                  key: 'number',
+                  width: 100,
                 },
-                {
-                  title: '功能点数',
-                  dataIndex: 'mNum'
-                }
-              ]
+              ],
             },
-            {
-              title: '复杂',
-              children: [
-                {
-                  title: '计数',
-                  dataIndex: 'cCount'
-                },
-                {
-                  title: '权重',
-                  dataIndex: 'cWeight'
-                },
-                {
-                  title: '功能点数',
-                  dataIndex: 'cNum'
-                }
-              ]
-            }
-          ]
-        },
-        {
-          title: '未调整功能点数',
-          dataIndex: 'noNum'
-        }
-      ],
-      tableData: [
-        {
-          component: 'EI',
-          num: '2',
-          eCount: '',
-          eWeight: '3',
-          eNum: '',
-          mCount: '',
-          mWeight: '4',
-          mNum: '',
-          cCount: '',
-          cWeight: '6',
-          cNum: '',
-          noNum: ''
-        },
-        {
-          component: 'EO',
-          num: '2',
-          eCount: '',
-          eWeight: '3',
-          eNum: '',
-          mCount: '',
-          mWeight: '4',
-          mNum: '',
-          cCount: '',
-          cWeight: '6',
-          cNum: '',
-          noNum: '',
-        },
-        {
-          component: 'EQ',
-          num: '2',
-          eCount: '',
-          eWeight: '3',
-          eNum: '',
-          mCount: '',
-          mWeight: '4',
-          mNum: '',
-          cCount: '',
-          cWeight: '6',
-          cNum: '',
-          noNum: '',
-        },
-        {
-          component: 'ILF',
-          num: '2',
-          eCount: '',
-          eWeight: '3',
-          eNum: '',
-          mCount: '',
-          mWeight: '4',
-          mNum: '',
-          cCount: '',
-          cWeight: '6',
-          cNum: '',
-          noNum: '',
-        },
-        {
-          component: 'EIF',
-          num: '2',
-          eCount: '',
-          eWeight: '3',
-          eNum: '',
-          mCount: '',
-          mWeight: '4',
-          mNum: '',
-          cCount: '',
-          cWeight: '6',
-          cNum: '',
-          noNum: '',
+          ],
         },
       ],
-    }
-  },
-  methods: {
-    created() {
-      this.gettableData()
     },
-    updated() {
-      // 用于防止表格合计行不显示
-      this.$nextTick(() => {
-        this.$refs['detailTable'].doLayout();
-      })
+    {
+      title: 'Company',
+      children: [
+        {
+          title: 'Company Address',
+          dataIndex: 'companyAddress',
+          key: 'companyAddress',
+          width: 200,
+        },
+        {
+          title: 'Company Name',
+          dataIndex: 'companyName',
+          key: 'companyName',
+        },
+      ],
     },
-    pdfHandle() {
-      window.open('/#/show', "_blank")
+    {
+      title: 'Gender',
+      dataIndex: 'gender',
+      key: 'gender',
+      width: 80,
+      fixed: 'right',
     },
-    getSummaries(param, val) {
-      const {columns, data} = param;
-      const sums = [];
-      columns.forEach((column, index) => {
-        if (index === 0) {
-          sums[index] = (() => {
-            // let el=<p>未调整功能点</p>
-          })();
-          return;
-        }
-        if (index === 11) {
-          sums[index] = (() => {
-            // let num=<p >￥{this.tableData[val].nonum.toFixed(2)}</p>
-            return num;
-          })();
-          return;
-        }
-      });
-      return sums;
+  ];
+  const data = [...Array(100)].map((_, i) => ({
+    key: i,
+    name: 'John Brown',
+    age: i + 1,
+    street: 'Lake Park',
+    building: 'C',
+    number: 2035,
+    companyAddress: 'Lake Street 42',
+    companyName: 'SoftLake Co',
+    gender: 'M',
+  }));
+  export default defineComponent({
+    setup() {
+      return {
+        data,
+        columns,
+      };
     },
-    count() {
-    }
-  }
-}
+  });
 </script>
 
-<style scoped>
-.title {
-  text-align: center;
-  font-family: sans-serif;
-  font-size: 30px;
-}
-
-.secondtitle {
-  text-indent: 2em;
-  font-weight: bold;
-  margin-left: 30px;
-  margin-right: 30px;
-}
-
-.content {
-  text-indent: 2em;
-  margin-left: 20px;
-  margin-right: 20px;
-}
-
-.guidance {
-  position: absolute;
-  right: 50px;
-  font-weight: bold;
-}
-</style>
