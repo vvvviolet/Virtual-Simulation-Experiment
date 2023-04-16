0b708053c5cab79abe66fd7ec33580d0c21fdfdd
cuizhenhao <1278493070@qq.com>
2023-04-16 19:34:16 +0800

    table

--- front/src/pages/exp1/Exp1_MARKII/index.vue
+++ front/src/pages/exp1/Exp1_MARKII/index.vue
@@ -1,8 +1,8 @@
 <template>
 
     <h1 class="title" >实验1 基于MARKII的小型软件项目规模度量实验
-      
-        <span>  <el-button  class="guidance" type="primary" text  @click="pdfHandle" ><el-icon size="25px"><Document /></el-icon>实验指导书下载</el-button></span> 
+
+        <span>  <el-button  class="guidance" type="primary" text  @click="pdfHandle" ><el-icon size="25px"><Document /></el-icon>实验指导书下载</el-button></span>
     </h1>
     <hr />
     <!-- <span> {{ test }}</span> -->
@@ -10,7 +10,7 @@
     <p class="content">理解软件项目规模度量功能点法原理，通过实验操作掌握功能点法。 学生应以小组为单位，根据本小组“软件工程管理与经济”课程设计项目架构及组件等设计成果，以功能点方法测量该项目的规模(功能点数量)。 建议选用某一种功能点方法度量课程设计项目的功能点，并采用另外一种功能点方法或其他的软件规模度量方法对前一种方法的度量结果进行验证。 本实验为课内设计性实验项目，实验学时 1 学时，完成实验报告 1 学时。
     </p>
     <h2>二、实验参数  </h2>
-    
+
     <el-table :data="tableData" border style="width: 100%"  :summary-method="data=>getSummaries(data,index)" show-summary ref="detailTable">
     <el-table-column prop="component" label="组件" width="80" />
     <el-table-column prop="number" label="数量" width="80" />
@@ -54,6 +54,13 @@
     <el-table-column prop="address" label="Address" /> -->
     </el-table>
 
+  <a-table
+      :columns="columns"
+      :data-source="data"
+      bordered
+      size="middle"
+  />
+
 
 
 </template>
@@ -63,9 +70,67 @@ import { Document } from '@element-plus/icons-vue'
 export default {
     name: 'Exp1_IFPUG',
     data() {
-        return{     
+        return{
             test:'21111',
             sum:'',
+            columns: [
+              {
+                title: '组件'
+              },
+              {
+                title: '数量'
+              },
+              {
+                title: '复杂度',
+                children: [
+                  {
+                    title: '简单',
+                    children: [
+                      {
+                        title: '计数'
+                      },
+                      {
+                        title: '权重'
+                      },
+                      {
+                        title: '功能点数'
+                      }
+                    ]
+                  },
+                  {
+                    title: '平均',
+                    children: [
+                      {
+                        title: '计数'
+                      },
+                      {
+                        title: '权重'
+                      },
+                      {
+                        title: '功能点数'
+                      }
+                    ]
+                  },
+                  {
+                    title: '复杂',
+                    children: [
+                      {
+                        title: '计数'
+                      },
+                      {
+                        title: '权重'
+                      },
+                      {
+                        title: '功能点数'
+                      }
+                    ]
+                  }
+                ]
+              },
+              {
+                title: '未调整功能点数'
+              }
+            ],
             tableData: [
                 {
                     component: 'EI',
@@ -150,7 +215,7 @@ export default {
         	   this.$refs['detailTable'].doLayout();
         	})
         },
-        pdfHandle(){        
+        pdfHandle(){
             window.open('/#/show',"_blank")
         },
         getSummaries(param,val){
