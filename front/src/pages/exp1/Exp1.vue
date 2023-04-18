f446def7178c28e399ff5bb8b5be2b32fe5cfe3d
Baokker <1048098862@qq.com>
2023-04-18 08:39:00 +0800

    try to add common download button and title in exp1

--- front/src/pages/exp1/Exp1.vue
+++ front/src/pages/exp1/Exp1.vue
@@ -1,13 +1,34 @@
 <!-- 1.软件规模估算实验（FP方法）
 包括:IFPUG方法、NESMA方法、MARK II方法、COSMIC方法 -->
 <template>
-      <RouterView/>
-  </template>
-  
-  <script lang="ts">
-    export default {
-      name: 'Exp1',
-    };
-  </script>
-  
-  <style scoped lang="less"></style>
+  <h1 class="title">
+    {{ $route.meta.title }}
+    <a-button class="button1" type="primary" shape="round">
+      <template #icon> <DownloadOutlined /> </template>实验指导书下载
+    </a-button>
+    <a-button class="button2" type="primary" shape="round">
+      <template #icon> <DownloadOutlined /> </template>实验报告模板下载
+    </a-button>
+    </h1>
+  <RouterView />
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Exp1',
+    mounted() {
+      this.$router.afterEach((to) => {
+        console.log(to);
+        document.title = to.title || '实验一';
+      });
+    },
+  };
+</script>
+
+<style scoped lang="less">
+  .title {
+    text-align: center;
+    font-family: sans-serif;
+    font-size: 30px;
+  }
+</style>
