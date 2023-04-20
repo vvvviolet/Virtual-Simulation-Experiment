a28e638da6c1ec10e2ece6aa77c1b2a1b63b4c01
Baokker <1048098862@qq.com>
2023-04-20 10:18:21 +0800

    fix a bug of exp6 decision tree

--- front/components.d.ts
+++ front/components.d.ts
@@ -23,6 +23,7 @@ declare module '@vue/runtime-core' {
     AMenu: typeof import('ant-design-vue/es')['Menu']
     AMenuItem: typeof import('ant-design-vue/es')['MenuItem']
     AModal: typeof import('ant-design-vue/es')['Modal']
+    APopconfirm: typeof import('ant-design-vue/es')['Popconfirm']
     APopover: typeof import('ant-design-vue/es')['Popover']
     ARadioButton: typeof import('ant-design-vue/es')['RadioButton']
     ARadioGroup: typeof import('ant-design-vue/es')['RadioGroup']
