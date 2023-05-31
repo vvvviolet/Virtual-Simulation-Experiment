55219ac3d6f5fbadcce30b7e3ff6fb088e953f2e
KEN <1610348804@qq.com>
2023-05-31 23:04:53 +0800

    修复无法显示页面的问题

--- front/src/pages/exp8/minganxing/index.vue
+++ front/src/pages/exp8/minganxing/index.vue
@@ -32,18 +32,19 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import * as echarts from 'echarts'
 import { use } from 'echarts/core'
 import { CanvasRenderer } from 'echarts/renderers'
 import { LegendComponent, TooltipComponent } from 'echarts/components'
-import InputChart from '@/pages/exp8/minganxing/SAM/InputChart.vue';
+import InputChart from '@/pages/exp8/MINGANXING/InputChart.vue';
 import { Table } from 'ant-design-vue';
 
 // 注册需要使用的 ECharts 组件
 use([CanvasRenderer, LegendComponent, TooltipComponent])
 
-export default Exp6_SAM {
+export default {
+  name: 'minganxing',
   components: {
     InputChart
   },
