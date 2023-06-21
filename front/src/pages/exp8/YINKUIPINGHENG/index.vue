b54f4d56351a9928d2d4bde7e71f0e6ab20b9d57
cxs-tj <83899840+cxs-tj@users.noreply.github.com>
2023-06-21 23:08:10 +0800

    Update index.vue

--- front/src/pages/exp8/YINKUIPINGHENG/index.vue
+++ front/src/pages/exp8/YINKUIPINGHENG/index.vue
@@ -1,5 +1,27 @@
 <template >
 <div id="pdfDom" style="margin-left: 10px;">
+    <div style="padding-top:60px;padding-bottom:20px">
+        <a-config-provider  >
+            <p style="line-height:200%;font-size: 16px;">
+                <a-row justify="center">
+                    <a-col span="6">课程名称：软件工程管理与经济</a-col>
+                    <a-col span="6">课号：420279</a-col>
+                    <a-col span="6">实验项目名称：盈亏平衡实验</a-col>
+                </a-row>
+                <a-row justify="center">
+                    <a-col span="6">实验时间：<span style="border-bottom: 1px solid grey;border-radius: none;"><a-date-picker
+                                v-model="experimentdate" :bordered="false"
+                                style="width:150px;padding-left:3px;padding-right:3px;"
+                                placeholder="点击选择实验时间" /></span></a-col>
+                    <a-col span="6">实验报告人： <span style="border-bottom: 1px solid grey;border-radius: none;"><a-input
+                                v-model="reportername" placeholder="请输入报告人姓名" size="small" :bordered="false"
+                                style="width:18vh;"></a-input></span>
+                    </a-col>
+                    <a-col span="6"></a-col>
+                </a-row>
+            </p>
+        </a-config-provider>
+      </div>
     <h2><b>一、实验目的 </b></h2>
     <!-- <p class = "secondtitle">1. 风险与不确定性</p>
     <p class = "content">决策分析通常分为有风险的选择和无风险的选择。
@@ -170,6 +192,8 @@ export default {
             feelings:'',//实验心得
             avg_safety_rate:'',//平均安全边际率
             tableData: [],
+            experimentdate: 0,//实验时间
+            reportername: '',//实验人姓名
 
             columns: [
               {
